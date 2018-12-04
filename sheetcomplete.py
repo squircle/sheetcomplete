c4418503ad52e48d2d8c24fd4ddacd0fd8d9236c
Noah Kruiper <nkruiper@gmail.com>
2018-12-03 20:24:10 -0500

    works end-to-end. updated sample csv to the first 10K rows of https://open.canada.ca/data/en/dataset/7b569daa-59cd-4c14-8ffe-6bc8ebe8d8bb

--- sheetcomplete.py
+++ sheetcomplete.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import math
 
-__author__      = "Noah Kruiper and Tyson Moore"
-__license__     = "TBD"
-__version__     = "0"
-__email__       = "nkrui088@uottawa.ca, tmoor092@uottawa.ca"
-__status__      = "Development"
+__author__ = "Noah Kruiper and Tyson Moore"
+__license__ = "TBD"
+__version__ = "1.0"
+__email__ = "nkrui088@uottawa.ca, tmoor092@uottawa.ca"
+__status__ = "Development"
 
-# SheetComplete main program 
+# SheetComplete
 # by Noah Kruiper and Tyson Moore
 # for Miodrag Bolic, CEG 4913 Fall 2018
 
@@ -20,40 +21,38 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
-import sklearn.utils
 import csv
 import sys
 import pandas
 import numpy
 import copy
+import random
 
 # ------------------------------------------------------------------------------
 #   Definitions
 # ------------------------------------------------------------------------------
 
 # random state for regressors
-# TODO: implement random chooser for this state and a corresponding flag
-rand_state = 4 # chosen by fair dice roll, guaranteed to be random
+rand_state = random.randint(0, 99)  # chosen by fair dice roll, guaranteed to be random
 
 # ------------------------------------------------------------------------------
 #   Functions
 # ------------------------------------------------------------------------------
 
-# Step -1: parse command-line arguments for things like:
-# - input and output filenames
-# - random number input for test/train split
-# - verbosity
-# TODO: method implementation
-# NOTE: hard-coding now is fine
 
-# open first argument as CSV
-filename = sys.argv[1]
-# indicates if the original format of the csv was column-oriented
-columnOriented = True
+# Step 0: parse command-line arguments
+def load_file():
+    global filename, column_oriented
+    # open first argument as CSV
+    filename = sys.argv[1]
+    print('\n========\nProcessing file: ' + filename + '\n========\n')
+    # indicates if the original format of the csv was column-oriented
+    column_oriented = True
 
-# Step 0: get the data out
+
 # Step 1: determine directionality
 def parse_csv():
+    global column_oriented
     """
     Parse the given CSV file to extract data in a usable format. At a high
     level, this involves the following steps:
@@ -67,37 +66,21 @@ def parse_csv():
         # Note: possibly unnecessary
         csvFile.seek(0)
         # If the csv is determined to have a header, then it's ready for analysis
-        if csv.Sniffer().has_header(csvFile.read(1024)):
-            dataframe = pandas.read_csv(filename)
+        if csv.Sniffer().has_header(csvFile.read(2)):
+            data_frame = pandas.read_csv(filename)
         else:
             # transpose the data to see if that reveals a header
             # todo: delete the transposed version after processing
             pandas.read_csv(filename).T.to_csv('transposed-' + filename, header=False)
             with open("transposed-" + filename, newline='') as csvFileFlipped:
                 if csv.Sniffer().has_header(csvFileFlipped.read(1024)):
-                    columnOriented = False
-                    dataframe = pandas.read_csv('transposed-' + filename)
+                    data_frame = pandas.read_csv('transposed-' + filename)
+                    column_oriented = False
                 else:
                     print('Unable to determine data orientation. Now exiting.')
-                    # todo: delete transposed file here?
                     exit()
-        return dataframe
+        return data_frame
 
-    # TODO: method completion
-
-
-# Step 2: determine data set type
-def get_data_types():
-    """
-    Intuit the extracted data fields to determine what type of data they
-    contain. For example, phone numbers, street names, and other discrete
-    categorizable data types.
-
-    This isn't a robust check, but it feeds into later steps to determine
-    correctness of the chosen machine learning algorithm.
-    """
-
-    # TODO: method completion
 
 # Step 3: sort and classify data sets
 # Step 4: train networks
@@ -133,16 +116,17 @@ def run_algos(x, y):
             "Y_test": Y_test_num
         })
 
-    print('\nTrain Data X:\n' + X_train.to_string())
-    print('\nTest Data X:\n' + X_test.to_string())
-    print('\nTrain Data Y:\n' + Y_train.to_string())
-    print('\nTest Data Y:\n' + Y_test.to_string())
+    # print('\nTrain Data X:\n' + X_train.to_string())
+    # print('\nTest Data X:\n' + X_test.to_string())
+    # print('\nTrain Data Y:\n' + Y_train.to_string())
+    # print('\nTest Data Y:\n' + Y_test.to_string())
 
     # run the regressors against the input data
     for algo in regressors_list:
         algo["regressor"].fit(X_train_num, Y_train_num)
 
     return regressors_list
+
 
 # Step 5: evaluate algorithms
 def evaluate_algos(reg_list):
@@ -160,17 +144,18 @@ def evaluate_algos(reg_list):
 
     # choose the best one and return it
     best_algo = None
-    print("\nScore report:\n=============\n\n")
+    print("\n-------- Score report: --------")
 
     for algo in reg_list:
         print("{}: {}".format(type(algo["regressor"]), algo["score"]))
-        if best_algo == None:
+        if best_algo is None:
             best_algo = algo
         elif algo["score"] > best_algo["score"]:
             best_algo = algo
 
     print("\nBest regressor: {}".format(type(best_algo["regressor"])))
     return best_algo
+
 
 def regressor_factory():
     """
@@ -188,69 +173,95 @@ def regressor_factory():
         MLPRegressor(hidden_layer_sizes=100, random_state=rand_state)
         # TODO: flesh out options -- this neural net will need a lot of them,
         #       maybe even a loop over multiple layer sizes, learning rates, etc.
-        ]
-    
+    ]
+
     return copy.deepcopy(regressors)
 
+
 # Step 6: fill in missing data
-def fill_missing(X, regressor):
+def fill_missing():
     """
     Fills in the missing data using the chosen regressor. X is the input, and will output Y,
     given the specified regressor.
     """
-    Y = regressor.predict(X) # FIXME: does this need to unpack the data frame somehow?
+    # fill missing data with predictions
+    # for every row with nulls:
+    for i in df_onlyNull.index:
+        # for every column with nulls:
+        for j in range(len(df_containNull)):
+            # make a prediction if the cell is empty
+            if df_containNull[j] and datatypes[j] != 'object':
+                if math.isnan(filled.iloc[i, j]):
+                    x = filled.drop(filled.iloc[:, j].name, axis=1).copy()
+                    x = x.iloc[i]
+                    # do some preliminary data processing to convert strings to float
+                    x = x.apply(pandas.to_numeric, errors='coerce')
+                    x.fillna(0, inplace=True)
+                    prediction = selected_regressors[j]['regressor'].predict(numpy.array([x]).reshape(-1, len(df_containNull)-1))
+                    # print('\nPredicted Data Y:\n' + str(prediction))
+                    filled.iloc[i, j] = prediction
 
-    return Y # FIXME: does this need to be of some dataframe type?
+    print('Writing output file: ' + 'filled-' + filename)
+    if not column_oriented:
+        filled.T.to_csv('filled-' + filename, header=False, index=False)
+    else:
+        filled.to_csv('filled-' + filename, header=True, index=False)
 
 # ------------------------------------------------------------------------------
 #   Main function
 # ------------------------------------------------------------------------------
 
 if __name__ == '__main__':
-    
+
     # treat inf and empty string as NA
     pandas.options.mode.use_inf_as_na = True
 
-    # STEP 1: Parse input CSV to dataframe
-    dataframe = parse_csv()
-    print('\nInput Dataframe:\n' + dataframe.to_string())
+    # STEP 0: Load input file
+    load_file()
 
-    # STEP 2:
-    datatypes = dataframe.dtypes # where object, treat as string
-    print('\nDatatypes:\n' + datatypes.to_string())
+    # STEP 1: Parse input CSV to dataframe
+    input_dataframe = parse_csv()
+    filled = copy.deepcopy(input_dataframe)
+    # print('\nInput Dataframe:\n' + input_dataframe.to_string())
+
+    # Step 2: determine data set type
+    datatypes = input_dataframe.dtypes  # where object, treat as string
+    # print('\nDatatypes:\n' + datatypes.to_string())
 
     # Store rows which have NO NULLs
-    df_noNull = dataframe.dropna()
-    print('\nRows with NO NULL cells:\n' + df_noNull.to_string())
+    df_noNull = input_dataframe.dropna()
+    # print('\nRows with NO NULL cells:\n' + df_noNull.to_string())
 
     # Store rows which have NULLs
-    df_onlyNull = dataframe[~dataframe.index.isin(df_noNull.index)]
-    print('\nRows WITH NULL cells:\n' + df_onlyNull.to_string())
+    df_onlyNull = input_dataframe[~input_dataframe.index.isin(df_noNull.index)]
+    # print('\nRows WITH NULL cells:\n' + df_onlyNull.to_string())
 
     # Indicate which columns have NULLs
     df_containNull = df_onlyNull.isna().any()
-    print('\nColumns with NULLs:\n' + df_containNull.to_string())
+    # print('\nColumns with NULLs:\n' + df_containNull.to_string())
 
-    # NOTE: index SHOULD be preserved with df_onlyNull, meaning it should be possible to iterate through at the end and...
-    # NOTE: ...fill in missing data by iterating through those indexes, recreating the original order and shape of the data.
+    # create list of regressors corresponding to columns
+    selected_regressors = []
 
     # Iterate through all columns
     for i in range(len(df_containNull)):
         # if the column has missing data
         if df_containNull[i]:
-            print('\n----------------\nColumn to train: ' + df_noNull.iloc[:, i].name)
+            print('\n========================\nColumn to train: ' + df_noNull.iloc[:, i].name)
             # strip the column from the dataframe
             y = df_noNull.iloc[:, i].copy()
-            print('\nTarget Column Y:\n' + y.to_string())
+            # print('\nTarget Column Y:\n' + y.to_string())
             x = df_noNull.drop(df_noNull.iloc[:, i].name, axis=1).copy()
-            print('\nInput Data X:\n' + x.to_string())
-            
+            # print('\nInput Data X:\n' + x.to_string())
+
             # run all the regressors
             reg_list = run_algos(x, y)
 
             # evaluate which is best
             best_algo = evaluate_algos(reg_list)
+            selected_regressors.append(best_algo)
+        else:
+            selected_regressors.append(None)
 
-            # fill missing data with predictions
-            # FIXME: do data frame maniupulations to call fill_missing and put the data back
-            # fill_missing(???, best_algo["regressor"])
+    # Fill empty numeric cells
+    fill_missing()
